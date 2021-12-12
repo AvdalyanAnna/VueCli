1f94f21fcbe82a7c1c72af3b7a882c8fa44d61b1
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2021-12-12 22:49:40 +0400

    fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,77 +1,12 @@
 <template>
   <div>
-    <div class="money">
-      <div class="money-item" v-for="(value, name, index) in MONEY" :key="index">
-        <div class="title">{{value.Name}}</div>
-        <div class="text">
-          <div class="price">
-            <div>1<span> {{name}} </span></div>
-            to
-            <div>{{ (1 / value.Now).toFixed(5) }} <span> RUB </span>
-            </div>
-          </div>
-          <div class="trend">{{ (value.Trend).toFixed(5) }}</div>
-        </div>
-      </div>
-    </div>
+
   </div>
 </template>
 
 <script>
-import {mapActions, mapGetters} from 'vuex'
-
 export default {
   name: "Home",
-  computed: {
-    ...mapGetters({
-      MONEY: 'getMoney'
-    })
-  },
-  mounted() {
-    this.SET_MONEY()
-  },
-  methods: {
-    ...mapActions({
-      SET_MONEY: 'setMoney'
-    })
-  }
-
 }
 </script>
 
-<style lang="scss">
-body {
-  background: dodgerblue;
-}
-
-.money {
-  &-item {
-    max-width: 500px;
-    width: 100%;
-    margin: 10px auto;
-    border-radius: 16px;
-    padding: 20px;
-    box-sizing: border-box;
-    background: #fff;
-
-    .title {
-      font-size: 24px;
-      line-height: 28px;
-      color: grey;
-    }
-
-    .text {
-      display: flex;
-      justify-content: space-between;
-      margin: 20px 0 0;
-      font-size: 20px;
-      line-height: 24px;
-      color: black;
-
-      .price {
-        display: flex;
-      }
-    }
-  }
-}
-</style>
--- src/store/money.js
+++ src/store/money.js
@@ -1,24 +1,11 @@
-import axios from "axios";
-import cbrXmlDailyRu from "@/mixon/cbrXmlDailyRu";
-
 export default {
     state: {
         money: [],
 
     },
     mutations: {
-        setMoney(state, payload) {
-            state.money = payload;
-        }
     },
     actions: {
-        setMoney({commit}) {
-            axios.get('https://www.cbr-xml-daily.ru/daily_json.js')
-                .then(res => {
-                    cbrXmlDailyRu(res.data.Valute)
-                    commit("setMoney", res.data.Valute);
-                })
-        }
     },
     getters: {
         getMoney(state) {
